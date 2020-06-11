d215aba7cae711a0941f4294ebe28b8f649ac39c
Aurelius <theaureliusking@gmail.com>
2020-06-12 00:35:14 +0300

    Fix macOS file limit issue

--- README.md
+++ README.md
@@ -1,31 +1,47 @@
 Collager
 =============
 
-A Python script for generating collages from PNG files.
+A Python script for generating a collage out of a set of PNG files. Compatible with Windows, OS X and Linux.
+
+Installation:
+------
+
+To run the script, you need to install Pillow (a fork of the Python Imaging Library, or PIL). 
+Installation guide here: https://pillow.readthedocs.io/en/stable/installation.html
+
+
 
 Usage:
 ------
+
+Below is a list of optional arguments. It is highly recommended to set up separate input and output folders.
+
 ```
 optional arguments:
   -h, --help            show this help message and exit
   -w MAX_WIDTH, --maxwidth MAX_WIDTH
                         maximum output width 
-						(default 1304)
+						(default: 1304)
   -p PADDING, --padding PADDING
                         gap size between images and edges 
-						(default 4)
+						(default: 4)
   -f FOLDERS, --folders FOLDERS
-                        paths to images (separate with ",")
+                        paths to images (separate multiple folders with a blank space)
+						(default: current working folder)
   -o OUTPUT, --output OUTPUT
                         path to output
+						(default: current working folder)
   -c COMP, --compression COMP
                         compression level (0-9) 
-						(default 6)
+						(default: 6)
+  -b BG_COLOR, --bgcolor BG_COLOR
+						determines background color in RGBA (separate each channel with a blank space
+						(default: 255 255 255 255)
 ```
 
 Examples:
 ```
 imagecollage.py -f imagefolder -o outputfolder
-imagecollage.py -f imf1,imf2,imf3 -o outputfolder -w 1936 -p 8 -c 3
+imagecollage.py -f imf1 imf2 imf3 -o outputfolder -w 1936 -p 8 -c 3 -b 0 0 0 255
 imagecollage.py -f C:/path/to/image/folder -o C:/path/to/output/folder
 ```
--- imagecollage.py
+++ imagecollage.py
@@ -13,12 +13,15 @@ import time
 import argparse
 import os
 
-def collager(max_width,padding,folders,output,comp):
+def collager(max_width,padding,folders,output,comp,bg_color):
     
     ## Open output image
     def openImage(path):
-        imageViewerFromCommandLine = {"win32":"explorer","darwin":"open"}[sys.platform]
-        subprocess.run([imageViewerFromCommandLine, path])
+        try:
+            imageViewerFromCommandLine = {"win32":"explorer","darwin":"open","linux":"xdg-open"}[sys.platform]
+            subprocess.run([imageViewerFromCommandLine, path])
+        except KeyError:
+            return
     
     ## Setting up
     images = []
@@ -27,16 +30,21 @@ def collager(max_width,padding,folders,output,comp):
     images_height.append(padding)
 
     ## Load images
+    image_index = 1
+    
     for folder_index in folders:
         for image_path in glob.glob(os.path.join(folder_index,"*.png")):
-            images.append(Image.open(image_path))
+            image = Image.open(image_path)
+            images.append((image_index,image_path,image.size[0],image.size[1]))
+            image_index += 1
+            image.close()
     
     ## Check for wide images
-    for img in images:
-        if (img.size[0]+padding*2)>max_width:
+    for _,_,width,_ in images:
+        if (width+padding*2)>max_width:
             print("Error: List contains images that are too wide!")
             return False
-
+    
     ## Check for proper compression value
     if comp not in range(0,10):
         print("Error: Invalid compression value!")
@@ -46,14 +54,14 @@ def collager(max_width,padding,folders,output,comp):
     current_width = padding
     img_h = 0
 
-    for index,image in enumerate(images):
-        if (current_width + image.size[0]+padding) > max_width:
+    for index,_,width,height in images:
+        if (current_width + width+padding) > max_width:
             images_height.append(img_h+padding)
             current_width = padding
             img_h = 0
-        current_width += image.size[0]+padding
-        if image.size[1]>img_h:
-            img_h = image.size[1]
+        current_width += width+padding
+        if height > img_h:
+            img_h = height
         if index == len(images)-1:
             images_height.append(img_h+padding)
     
@@ -63,16 +71,20 @@ def collager(max_width,padding,folders,output,comp):
     ## Generating the collage
     current_width = padding
     current_height_index = 0
+    current_image_index = 0
     
     print("Processing images...")
     
-    background = Image.new("RGB",(max_width,output_height),(250,250,250,0))
-    for index,image in enumerate(images):
-        if (current_width + image.size[0]+padding) > max_width:
+    background = Image.new("RGBA",(max_width,output_height),(bg_color[0],bg_color[1],bg_color[2],bg_color[3]))
+    for index,path,width,_ in images:
+        if (current_width + width+padding) > max_width:
             current_height_index += 1
             current_width = padding
+        image = Image.open(path)
         background.paste(image,(current_width,images_cumul_height[current_height_index]),image.convert("RGBA"))
-        current_width += image.size[0]+padding
+        image.close()
+        current_image_index += 1
+        current_width += width+padding
         
     print("Saving collage...")
     if not os.path.exists(output):
@@ -89,15 +101,16 @@ def main():
     parse = argparse.ArgumentParser(description="Collage maker.")
     parse.add_argument('-w','--maxwidth', dest='max_width',type=int, default=1304, help='maximum output width')
     parse.add_argument('-p','--padding', dest='padding',type=int, default=4, help='gap size between images and edges')
-    parse.add_argument('-f','--folders', dest='folders', type=lambda s: [str(item) for item in s.split(',')], default=".", help='paths to images (separate with ",")')
+    parse.add_argument('-f','--folders', nargs='+', dest='folders', type=str, default=".", help='paths to images')
     parse.add_argument('-o','--output', dest='output', type=str, default=".", help='path to output')
     parse.add_argument('-c','--compression', dest='comp', type=int, default=6, help='compression level (0-9)')
+    parse.add_argument('-b','--bgcolor', nargs='+', dest='bg_color', type=int, default=(255,255,255,255), help='background color in RGBA')
 
     args = parse.parse_args()
     
     ## Running Collager function
     start = time.time()
-    proc = collager(args.max_width,args.padding,args.folders,args.output,args.comp)
+    proc = collager(args.max_width,args.padding,args.folders,args.output,args.comp,args.bg_color)
     if not proc:
         print("Failed to create collage!")
         sys.exit(1)
